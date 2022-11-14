ef76e72b148a07564cb483036492221737e7aead
bratuella1 <117741263+bratuella1@users.noreply.github.com>
2022-11-15 00:51:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿//************************
+// практическая работа №7*
+// выполнил: Ханов Артур *
+//************************
+using System;
+
+namespace PR_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x,z,v,c;//объявление переменных
+            uint i ;
+            ulong fact;
+            Console.WriteLine("здравствуйте!");
+            Console.Write("введите число x = ");
+            x = Convert.ToDouble(Console.ReadLine());
+            fact = 1;
+            c = 0;
+
+
+                for (i = 3; i <= 15; i += 2)// цикл для решения задачи
+                {
+                    fact = fact * (i - 1) * i;// расчет факториала
+                    Console.WriteLine(fact);
+                    z = Math.Pow(x, i);
+                    Console.WriteLine($"факториал 1  {z}");
+                    v = z / fact;
+                    Console.WriteLine($"факториал 2  {v}");
+                    c += v;
+                }
+            Console.WriteLine($"x равен  {c}");
+            Console.ReadLine();
+        }
+    }
+}
